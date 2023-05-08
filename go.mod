3e66db346050022f57bf72d62a17331e5ad1e761
Tiago Angelo <tiago.angelo@totvs.com.br>
2023-05-08 14:12:49 -0300

    Add command-line flags and Generator interface for code generation

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/totvs-cloud/pflagstruct
 
 go 1.20
+
+require (
+	github.com/enescakir/emoji v1.0.0
+	github.com/gookit/color v1.5.3
+	github.com/lmittmann/tint v0.3.2
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/cobra v1.7.0
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/enescakir/emoji"
+	"github.com/gookit/color"
+	"github.com/lmittmann/tint"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"golang.org/x/exp/slog"
+)
+
+var (
+	directory, structName, destination string
+	debug                              bool
+)
+
+type Generator interface {
+	Generate(directory string, structName string, destination string) error
+}
+
+func NewGenerator() Generator {
+	// TODO: implement me
+	panic("implement me")
+}
+
+func NewCommand() (*cobra.Command, error) {
+	cmd := &cobra.Command{
+		Use:           "flagstruct",
+		SilenceErrors: true,
+		SilenceUsage:  true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			generator := NewGenerator()
+			return generator.Generate(directory, structName, destination)
+		},
+	}
+
+	const (
+		directoryFlagName   = "directory"
+		structNameFlagName  = "struct-name"
+		destinationFlagName = "destination"
+		debugFlagName       = "debug"
+	)
+
+	cmd.Flags().StringVar(&directory, directoryFlagName, "", "Specifies the path where the tool will search for the source file containing the struct definition")
+	cmd.Flags().StringVar(&structName, structNameFlagName, "", "Specifies the name of the struct that will be generated in the code. This flag is required.")
+	cmd.Flags().StringVar(&destination, destinationFlagName, "", "Specifies the file name and path where the generated code will be saved. This flag is required.")
+	cmd.Flags().BoolVar(&debug, destinationFlagName, false, "Enables debug mode, which will print additional information during the code generation process to help with troubleshooting. If this flag is not set, the tool will run in normal mode without additional debug information.")
+
+	if err := cmd.MarkFlagRequired(directoryFlagName); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if err := cmd.MarkFlagRequired(structNameFlagName); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	if err := cmd.MarkFlagRequired(destinationFlagName); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return cmd, nil
+}
+
+func fatal(err error) {
+	if debug {
+		color.Redf("%s %+v", emoji.CrossMark, err)
+		os.Exit(1)
+	}
+
+	color.Redf("%s %s", emoji.CrossMark, err)
+	os.Exit(1)
+}
+
+func main() {
+	options := tint.Options{TimeFormat: time.Kitchen}
+	if debug {
+		options.AddSource = true
+		options.Level = slog.LevelDebug
+	}
+
+	slog.SetDefault(slog.New(options.NewHandler(os.Stderr)))
+
+	cmd, err := NewCommand()
+	if err != nil {
+		fatal(err)
+	}
+
+	if err = cmd.Execute(); err != nil {
+		fatal(err)
+	}
+}
