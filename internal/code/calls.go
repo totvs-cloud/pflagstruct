e063204fc2806de02ac94549d9e7e8218a0c5464
Tiago Angelo <131211366+tiagoangelototvs@users.noreply.github.com>
2023-06-02 10:48:07 -0300

    Enable the use of nullable struct references (#4)
    * Add PointerGetterCall struct for getting pointer values in GetterMethod
    
    * Fix the check statement
    
    * Return a new instance of the struct if is nil
    
    * Consider pointer fields
    
    * Use logical AND instead of OR in PointerGetterCall function

--- internal/code/calls.go
+++ internal/code/calls.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/dave/jennifer/jen"
 	changecase "github.com/ku/go-change-case"
+
 	"github.com/totvs-cloud/pflagstruct/projscan"
 )
 
@@ -122,4 +123,97 @@ func (g *GetterCall) Statement() *jen.Statement {
 	}
 
 	return nil
+}
+
+type PointerGetterCall struct {
+	Prefix  string
+	Struct  *projscan.Struct
+	Pointer bool
+	Field   *projscan.Field
+}
+
+func (g *PointerGetterCall) CobraMethod() string {
+	var suffix string
+	if g.Field.Array {
+		suffix = "Slice"
+	}
+
+	return changecase.Pascal(path.Join("get", g.Field.Type.String(), suffix))
+}
+
+func (g *PointerGetterCall) Flag() string {
+	return changecase.Param(path.Join(g.Prefix, g.Field.Name))
+}
+
+func (g *PointerGetterCall) Statement() *jen.Statement {
+	structName := changecase.Camel(g.Struct.Name)
+	fieldName := g.Field.Name
+	flagValue := "flagValue"
+
+	returnId := jen.Nil()
+	if !g.Pointer {
+		returnId = jen.Id(structName)
+	}
+
+	switch KindOf(g.Field) {
+	case FieldKindNative:
+		return jen.If(jen.List(jen.Id(flagValue), jen.Err()).Op(":=").
+			Id("cf").Dot("flags").Dot(g.CobraMethod()).Call(jen.Lit(g.Flag())), jen.Err().Op("!=").Nil()).
+			Block(
+				jen.Return().List(returnId, jen.Qual("fmt", "Errorf").Call(jen.Lit("error retrieving \""+g.Flag()+"\" from command flags: %w"), jen.Err())),
+			).Else().If(g.CompareToDefaultValue(jen.Id(flagValue).Op("!=")).Op("&&").Id(structName).Op("==").Nil()).
+			Block(
+				jen.Id(structName).Op("=").Op("&").Qual(g.Struct.Package.Path, g.Struct.Name).Values(jen.Id(fieldName).Op(":").Id(flagValue)),
+			).Else().If(g.CompareToDefaultValue(jen.Id(flagValue).Op("!="))).
+			Block(
+				jen.Id(structName).Dot(fieldName).Op("=").Id(flagValue),
+			)
+	case FieldKindStruct, FieldKindTCloudTag, FieldKindStringMap:
+		if g.Field.Pointer {
+			return jen.If(jen.List(jen.Id(flagValue), jen.Err()).Op(":=").
+				Id("cf").Dot(changecase.Camel(path.Join("Get", g.Prefix, fieldName))).Call(), jen.Err().Op("!=").Nil()).
+				Block(
+					jen.Return().List(returnId, jen.Err()),
+				).Else().If(g.CompareToDefaultValue(jen.Id(flagValue).Op("!=")).Op("&&").Id(structName).Op("==").Nil()).
+				Block(
+					jen.Id(structName).Op("=").Op("&").Qual(g.Struct.Package.Path, g.Struct.Name).Values(jen.Id(fieldName).Op(":").Id(flagValue)),
+				).Else().If(g.CompareToDefaultValue(jen.Id(flagValue).Op("!="))).
+				Block(
+					jen.Id(structName).Dot(fieldName).Op("=").Id(flagValue),
+				)
+		} else {
+			return jen.If(jen.List(jen.Id(flagValue), jen.Err()).Op(":=").
+				Id("cf").Dot(changecase.Camel(path.Join("Get", g.Prefix, fieldName))).Call(), jen.Err().Op("!=").Nil()).
+				Block(
+					jen.Return().List(returnId, jen.Err()),
+				).Else().If(jen.Id(structName).Op("==").Nil()).
+				Block(
+					jen.Id(structName).Op("=").Op("&").Qual(g.Struct.Package.Path, g.Struct.Name).Values(jen.Id(fieldName).Op(":").Id(flagValue)),
+				).Else().
+				Block(
+					jen.Id(structName).Dot(fieldName).Op("=").Id(flagValue),
+				)
+		}
+	}
+
+	return nil
+}
+
+func (g *PointerGetterCall) CompareToDefaultValue(statement *jen.Statement) *jen.Statement {
+	if g.Field.Array {
+		return statement.Nil()
+	}
+
+	switch g.Field.Type {
+	case projscan.FieldTypeString:
+		return statement.Lit("")
+	case projscan.FieldTypeBool:
+		return statement.False()
+	case projscan.FieldTypeFloat32, projscan.FieldTypeFloat64:
+		return statement.Lit(0.0)
+	case projscan.FieldTypeInt, projscan.FieldTypeInt8, projscan.FieldTypeInt16, projscan.FieldTypeInt32, projscan.FieldTypeInt64:
+		return statement.Lit(0)
+	default:
+		return statement.Nil()
+	}
 }
